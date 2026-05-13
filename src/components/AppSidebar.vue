0688caba01811a9b93a8e07d462b65ccce5f7a4d
xyrygcl <3254810549@qq.com>
2026-05-13 23:27:51 +0800

    Update AppSidebar.vue

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,8 +1,5 @@
 <script setup>
 import { RouterLink } from 'vue-router'
-
-import { logo } from '@/assets/brand/logo'
-import { sygnet } from '@/assets/brand/sygnet'
 import { AppSidebarNav } from '@/components/AppSidebarNav.js'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
@@ -18,16 +15,22 @@ const sidebar = useSidebarStore()
     :visible="sidebar.visible"
     @visible-change="(value) => sidebar.toggleVisible(value)"
   >
+    <!-- Sidebar Header -->
     <CSidebarHeader class="border-bottom">
       <RouterLink custom to="/" v-slot="{ href, navigate }">
         <CSidebarBrand v-bind="$attrs" as="a" :href="href" @click="navigate">
-          <CIcon custom-class-name="sidebar-brand-full" :icon="logo" :height="32" />
-          <CIcon custom-class-name="sidebar-brand-narrow" :icon="sygnet" :height="32" />
+          <!-- 替换 logo 显示文字 -->
+          <span class="sidebar-brand-full">MyApp</span>
+          <span class="sidebar-brand-narrow">MA</span>
         </CSidebarBrand>
       </RouterLink>
       <CCloseButton class="d-lg-none" dark @click="sidebar.toggleVisible()" />
     </CSidebarHeader>
+
+    <!-- Sidebar Navigation -->
     <AppSidebarNav />
+
+    <!-- Sidebar Footer -->
     <CSidebarFooter class="border-top d-none d-lg-flex">
       <CSidebarToggler @click="sidebar.toggleUnfoldable()" />
     </CSidebarFooter>
