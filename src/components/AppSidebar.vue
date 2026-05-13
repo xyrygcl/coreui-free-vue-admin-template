ecbccfa54383fbdfec6b76a318bd6d86647bba54
xyrygcl <3254810549@qq.com>
2026-05-14 00:25:44 +0800

    Update AppSidebar.vue

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { RouterLink } from 'vue-router'
 import { AppSidebarNav } from '@/components/AppSidebarNav.js'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
@@ -15,22 +14,14 @@ const sidebar = useSidebarStore()
     :visible="sidebar.visible"
     @visible-change="(value) => sidebar.toggleVisible(value)"
   >
-    <!-- Sidebar Header -->
-    <CSidebarHeader class="border-bottom">
-      <RouterLink custom to="/" v-slot="{ href, navigate }">
-        <CSidebarBrand v-bind="$attrs" as="a" :href="href" @click="navigate">
-          <!-- 替换 logo 显示文字 -->
-          <span class="sidebar-brand-full">MyApp</span>
-          <span class="sidebar-brand-narrow">MA</span>
-        </CSidebarBrand>
-      </RouterLink>
-      <CCloseButton class="d-lg-none" dark @click="sidebar.toggleVisible()" />
+    <CSidebarHeader class="border-bottom text-center py-3">
+      <h4 class="text-white m-0">
+        My Admin
+      </h4>
     </CSidebarHeader>
 
-    <!-- Sidebar Navigation -->
     <AppSidebarNav />
 
-    <!-- Sidebar Footer -->
     <CSidebarFooter class="border-top d-none d-lg-flex">
       <CSidebarToggler @click="sidebar.toggleUnfoldable()" />
     </CSidebarFooter>
