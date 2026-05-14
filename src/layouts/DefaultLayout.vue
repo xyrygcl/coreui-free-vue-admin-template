d93814726add73409e904fe5b339aa658d121285
xyrygcl <3254810549@qq.com>
2026-05-14 12:01:41 +0800

    Update DefaultLayout.vue

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,15 +1,33 @@
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+import AppSidebar from '@/components/AppSidebar.vue'
+</script>
+
 <template>
   <div>
+    <!-- 左侧菜单 -->
     <AppSidebar />
 
-    <div class="wrapper d-flex flex-column min-vh-100">
+    <!-- 右侧主体 -->
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+      <!-- 顶部导航 -->
       <AppHeader />
 
-      <div class="body flex-grow-1">
-        <CContainer class="px-4 lg">
-          <router-view />
-        </CContainer>
+      <!-- 页面内容 -->
+      <div class="body flex-grow-1 px-3 py-3">
+        <router-view />
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.wrapper {
+  margin-left: 256px;
+  background: #f4f5f7;
+}
+
+.body {
+  min-height: 100vh;
+}
+</style>
