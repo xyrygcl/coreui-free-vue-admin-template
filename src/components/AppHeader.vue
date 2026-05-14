f684b3e2c63c7636c682a4c1ce014b364a3fb7e5
xyrygcl <3254810549@qq.com>
2026-05-14 11:12:07 +0800

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,39 +3,26 @@ import { ref, onMounted } from 'vue'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
 const sidebar = useSidebarStore()
-
-const headerClassNames = ref('mb-4 p-0')
-
-onMounted(() => {
-  document.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > 0) {
-      headerClassNames.value = 'mb-4 p-0 shadow-sm'
-    } else {
-      headerClassNames.value = 'mb-4 p-0'
-    }
-  })
-})
+const headerClassNames = ref('mb-4 p-0 shadow-sm')
 </script>
 
 <template>
-  <CHeader position="sticky" :class="headerClassNames">
-    <CContainer class="border-bottom px-4" fluid>
-
-      <CHeaderToggler
-        @click="sidebar.toggleVisible()"
-        style="margin-inline-start: -14px"
-      >
+  <CHeader :class="headerClassNames" position="sticky">
+    <CContainer fluid class="d-flex justify-content-between align-items-center px-4">
+      <CHeaderToggler @click="sidebar.toggleVisible()">
         <CIcon icon="cil-menu" size="lg" />
       </CHeaderToggler>
 
-      <CHeaderNav class="d-none d-md-flex">
+      <h3 class="m-0">沃转课</h3>
+
+      <!-- 右上角铃铛 -->
+      <CNav class="d-flex align-items-center">
         <CNavItem>
-          <CNavLink href="/dashboard">
-            Dashboard
+          <CNavLink href="#">
+            <CIcon icon="cil-bell" size="lg" />
           </CNavLink>
         </CNavItem>
-      </CHeaderNav>
-
+      </CNav>
     </CContainer>
   </CHeader>
 </template>
