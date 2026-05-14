21370d683d9b23df6c1597b88b5b131c72ffc9dc
xyrygcl <3254810549@qq.com>
2026-05-14 12:10:31 +0800

    Update Dashboard.vue

--- src/views/dashboard/Dashboard.vue
+++ src/views/dashboard/Dashboard.vue
@@ -1,12 +1,58 @@
+<script setup>
+import {
+  CContainer,
+  CCard,
+  CCardBody,
+  CRow,
+  CCol,
+} from '@coreui/vue'
+</script>
+
 <template>
-  <Container fluid class="p-4">
-    <Card class="bg-dark text-light border-0 shadow">
+  <CContainer fluid class="p-4">
+    <CCard class="bg-dark text-light border-0 shadow-lg">
       <CCardBody>
-        <h3>欢迎使用沃转课</h3>
-        <p class="text-medium-emphasis">
-          CoreUI 模板已成功清理
+        <h2 class="mb-3">欢迎使用沃转课</h2>
+
+        <p class="text-medium-emphasis mb-4">
+          课程上传与作品管理平台
         </p>
+
+        <CRow class="g-4">
+          <CCol md="4">
+            <CCard class="bg-secondary text-light border-0">
+              <CCardBody>
+                <h5>立即上传</h5>
+                <p class="mb-0">
+                  上传你的课程内容
+                </p>
+              </CCardBody>
+            </CCard>
+          </CCol>
+
+          <CCol md="4">
+            <CCard class="bg-secondary text-light border-0">
+              <CCardBody>
+                <h5>我的作品</h5>
+                <p class="mb-0">
+                  管理你的课程资源
+                </p>
+              </CCardBody>
+            </CCard>
+          </CCol>
+
+          <CCol md="4">
+            <CCard class="bg-secondary text-light border-0">
+              <CCardBody>
+                <h5>个人中心</h5>
+                <p class="mb-0">
+                  查看账号信息
+                </p>
+              </CCardBody>
+            </CCard>
+          </CCol>
+        </CRow>
       </CCardBody>
-    </Card>
-  </Container>
+    </CCard>
+  </CContainer>
 </template>
