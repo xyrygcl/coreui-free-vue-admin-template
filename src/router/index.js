e22a0586b3fce598c3a10878ef54cc0138477cff
xyrygcl <3254810549@qq.com>
2026-05-14 11:27:46 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,21 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-import DefaultLayout from '@/layouts/DefaultLayout'
+// Views
+import Dashboard from '@/views/dashboard/Dashboard.vue'
+import Upload from '@/views/pages/Upload.vue'
+import Courses from '@/views/pages/Courses.vue'
+import Profile from '@/views/pages/Profile.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: DefaultLayout,
-    children: [
-      {
-        path: '',
-        name: 'Dashboard',
-        component: () => import('@/views/dashboard/Dashboard.vue'),
-      },
-    ],
-  },
+  { path: '/', redirect: '/dashboard' },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/upload', name: 'Upload', component: Upload },
+  { path: '/courses', name: 'Courses', component: Courses },
+  { path: '/profile', name: 'Profile', component: Profile },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
