5d364c0dbd8051a5358c13808243a8fce67ad69e
xyrygcl <3254810549@qq.com>
2026-05-14 11:33:47 +0800

    Create Profile.vue

---
+++ src/views/pages/Profile.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <CContainer fluid class="p-4 bg-white">
+    <CCard>
+      <CCardHeader>个人</CCardHeader>
+      <CCardBody>
+        <p class="text-secondary">这里是个人信息页面，你可以显示用户名、头像和账户信息等。</p>
+      </CCardBody>
+    </CCard>
+  </CContainer>
+</template>
+
+<style scoped>
+.bg-white { background-color: #ffffff; }
+</style>
