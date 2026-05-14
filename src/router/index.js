9f480589cc27c4466039c205cd8a7a34a8941304
xyrygcl <3254810549@qq.com>
2026-05-14 11:39:26 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-// Views
-import Dashboard from '@/views/dashboard/Dashboard.vue'
-import Upload from '@/views/pages/Upload.vue'
-import Courses from '@/views/pages/Courses.vue'
-import Profile from '@/views/pages/Profile.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
 
 const routes = [
-  { path: '/', redirect: '/dashboard' },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/upload', name: 'Upload', component: Upload },
-  { path: '/courses', name: 'Courses', component: Courses },
-  { path: '/profile', name: 'Profile', component: Profile },
+  {
+    path: '/',
+    name: '首页',
+    component: DefaultLayout,
+    redirect: '/dashboard',
+
+    children: [
+      {
+        path: 'dashboard',
+        name: '主页',
+        component: () => import('@/views/dashboard/App.vue'),
+      },
+
+      {
+        path: 'upload',
+        name: '立即上传',
+        component: () => import('@/views/pages/Upload.vue'),
+      },
+
+      {
+        path: 'courses',
+        name: '我的作品',
+        component: () => import('@/views/pages/Courses.vue'),
+      },
+
+      {
+        path: 'profile',
+        name: '个人中心',
+        component: () => import('@/views/pages/Profile.vue'),
+      },
+    ],
+  },
 ]
 
 const router = createRouter({
