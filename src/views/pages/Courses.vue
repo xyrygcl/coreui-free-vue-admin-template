dccf2b9a204c02129fb5a0c476e3e8e08bf09fe3
xyrygcl <3254810549@qq.com>
2026-05-14 11:33:19 +0800

    Create Courses.vue

---
+++ src/views/pages/Courses.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <CContainer fluid class="p-4 bg-white">
+    <CCard>
+      <CCardHeader>我的作品</CCardHeader>
+      <CCardBody>
+        <p class="text-secondary">这里是你的作品列表或课程页面，可以展示你的作品或课程信息。</p>
+      </CCardBody>
+    </CCard>
+  </CContainer>
+</template>
+
+<style scoped>
+.bg-white { background-color: #ffffff; }
+</style>
