4e6f8d305a88c5d1021eacd628c40213d48eb758
xyrygcl <3254810549@qq.com>
2026-05-14 23:34:19 +0800

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,19 +3,33 @@ import { ref } from 'vue'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
 const sidebar = useSidebarStore()
-const headerClassNames = ref('p-0 border-bottom border-light')
+
+const headerClassNames = ref(
+  'p-0 border-bottom border-dark'
+)
 </script>
 
 <template>
-  <CHeader :class="headerClassNames" position="sticky">
-    <CContainer fluid class="header-container px-4">
+  <CHeader
+    :class="headerClassNames"
+    position="sticky"
+  >
+    <CContainer
+      fluid
+      class="header-container px-4"
+    >
       <!-- 左侧菜单按钮 -->
-      <CHeaderToggler class="menu-btn" @click="sidebar.toggleVisible()">
+      <CHeaderToggler
+        class="menu-btn"
+        @click="sidebar.toggleVisible()"
+      >
         <CIcon icon="cil-menu" size="lg" />
       </CHeaderToggler>
 
       <!-- 中间标题 -->
-      <h3 class="header-title">沃转课</h3>
+      <h3 class="header-title">
+        沃转课
+      </h3>
 
       <!-- 右侧铃铛 -->
       <CNav class="header-right">
@@ -32,28 +46,34 @@ const headerClassNames = ref('p-0 border-bottom border-light')
 <style scoped>
 .header-container {
   position: relative;
+
   display: flex;
   align-items: center;
+
   height: 64px;
-  background: #ffffff;   /* 白色背景 */
-  border-bottom: 1px solid #e0e0e0; /* 浅灰色分隔线 */
+
+  background: #111827;
 }
 
 /* 左侧按钮 */
 .menu-btn {
-  color: #333333;    /* 深色图标 */
+  color: white;
   z-index: 2;
 }
 
 /* 中间标题 */
 .header-title {
   position: absolute;
+
   left: 50%;
   transform: translateX(-50%);
+
   margin: 0;
+
   font-size: 30px;
   font-weight: 700;
-  color: #333333;    /* 深色文字 */
+
+  color: white;
 }
 
 /* 右侧区域 */
@@ -62,6 +82,6 @@ const headerClassNames = ref('p-0 border-bottom border-light')
 }
 
 .header-right a {
-  color: #333333;    /* 深色图标 */
+  color: white;
 }
 </style>
