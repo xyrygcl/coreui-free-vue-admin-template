605f2c04bfcbf860c412e5d7b26d3b2bf2dc7a58
xyrygcl <3254810549@qq.com>
2026-05-14 11:32:57 +0800

    Create Upload.vue

---
+++ src/views/pages/Upload.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <CContainer fluid class="p-4 bg-white">
+    <CCard>
+      <CCardHeader>立即上传</CCardHeader>
+      <CCardBody>
+        <p class="text-secondary">这里是上传文件或作品的页面。你可以添加上传组件或表单。</p>
+      </CCardBody>
+    </CCard>
+  </CContainer>
+</template>
+
+<style scoped>
+.bg-white { background-color: #ffffff; }
+</style>
