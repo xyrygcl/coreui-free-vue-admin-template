f6f428d3d68922d994c1633bdfb713a0e481c6fc
xyrygcl <3254810549@qq.com>
2026-05-14 23:36:12 +0800

    Update sidebar.js

--- src/stores/sidebar.js
+++ src/stores/sidebar.js
@@ -1,16 +1,17 @@
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useSidebarStore = defineStore('sidebar', {
-  state: () => ({
-    unfoldable: false,
-    visible: true,
-  }),
-  actions: {
-    toggleUnfoldable() {
-      this.unfoldable = !this.unfoldable
-    },
-    toggleVisible(value) {
-      this.visible = value
-    },
-  },
+export const useSidebarStore = defineStore('sidebar', () => {
+  const visible = ref(undefined)
+  const unfoldable = ref(false)
+
+  const toggleVisible = (value) => {
+    visible.value = value !== undefined ? value : !visible.value
+  }
+
+  const toggleUnfoldable = () => {
+    unfoldable.value = !unfoldable.value
+  }
+
+  return { visible, unfoldable, toggleVisible, toggleUnfoldable }
 })
