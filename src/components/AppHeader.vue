87cbb24c6a8d01c912ac9a56f51f482637b6a8c6
xyrygcl <3254810549@qq.com>
2026-05-14 12:23:27 +0800

    Update AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,19 +3,36 @@ import { ref } from 'vue'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
 const sidebar = useSidebarStore()
-const headerClassNames = ref('mb-4 p-0 shadow-sm')
+
+const headerClassNames = ref(
+  'p-0 border-bottom border-dark'
+)
 </script>
 
 <template>
-  <CHeader :class="headerClassNames" position="sticky">
-    <CContainer fluid class="d-flex justify-content-between align-items-center px-4">
-      <CHeaderToggler @click="sidebar.toggleVisible()">
+  <CHeader
+    :class="headerClassNames"
+    position="sticky"
+  >
+    <CContainer
+      fluid
+      class="header-container px-4"
+    >
+      <!-- 左侧菜单按钮 -->
+      <CHeaderToggler
+        class="menu-btn"
+        @click="sidebar.toggleVisible()"
+      >
         <CIcon icon="cil-menu" size="lg" />
       </CHeaderToggler>
 
-      <h3 class="m-0">沃转课</h3>
+      <!-- 中间标题 -->
+      <h3 class="header-title">
+        沃转课
+      </h3>
 
-      <CNav class="d-flex align-items-center">
+      <!-- 右侧铃铛 -->
+      <CNav class="header-right">
         <CNavItem>
           <CNavLink href="#">
             <CIcon icon="cil-bell" size="lg" />
@@ -25,3 +42,46 @@ const headerClassNames = ref('mb-4 p-0 shadow-sm')
     </CContainer>
   </CHeader>
 </template>
+
+<style scoped>
+.header-container {
+  position: relative;
+
+  display: flex;
+  align-items: center;
+
+  height: 64px;
+
+  background: #111827;
+}
+
+/* 左侧按钮 */
+.menu-btn {
+  color: white;
+  z-index: 2;
+}
+
+/* 中间标题 */
+.header-title {
+  position: absolute;
+
+  left: 50%;
+  transform: translateX(-50%);
+
+  margin: 0;
+
+  font-size: 30px;
+  font-weight: 700;
+
+  color: white;
+}
+
+/* 右侧区域 */
+.header-right {
+  margin-left: auto;
+}
+
+.header-right a {
+  color: white;
+}
+</style>
